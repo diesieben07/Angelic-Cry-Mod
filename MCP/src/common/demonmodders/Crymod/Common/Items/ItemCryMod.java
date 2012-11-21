80f2ec17e13020a043c99807cf179ca85041fc51
Resinresin <resinresinl@gmail.com>
2012-11-21 22:01:39 +0000

    Added the first 4 crystals.

--- MCP/src/common/demonmodders/Crymod/Common/Items/ItemCryMod.java
+++ MCP/src/common/demonmodders/Crymod/Common/Items/ItemCryMod.java
@@ -9,6 +9,10 @@ import net.minecraft.src.Item;
 public class ItemCryMod extends Item {
 
 	public static Item summoningBook;
+	public static Item crystalYellow;
+	public static Item crystalRed;
+	public static Item crystalLightBlue;
+	public static Item crystalPurple;
 	
 	public ItemCryMod(int itemId) {
 		super(itemId);
@@ -21,6 +25,11 @@ public class ItemCryMod extends Item {
 	
 	public static void createItems() {
 		summoningBook = new ItemSummoningBook(Crymod.conf.getItem("summoningBookId", 4765).getInt());
+		crystalYellow = new ItemCrystalYellow(Crymod.conf.getItem("crystalYellowId", 4767).getInt());
+		crystalRed = new ItemCrystalRed(Crymod.conf.getItem("crystalRedId", 4768).getInt());
+		crystalLightBlue = new ItemCrystalLightBlue(Crymod.conf.getItem("crystalLightBlueId", 4769).getInt());
+		crystalPurple = new ItemCrystalPurple(Crymod.conf.getItem("crystalPurpleId", 4770).getInt());
+		
 	}
 
 	@Override
---
+++ MCP/src/common/demonmodders/Crymod/Common/Items/ItemCrystalLightBlue.java
@@ -0,0 +1,22 @@
+package demonmodders.Crymod.Common.Items;
+
+import demonmodders.Crymod.Common.Crymod;
+import demonmodders.Crymod.Common.Gui.GuiType;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.World;
+
+public class ItemCrystalLightBlue extends ItemCryMod {
+
+	public ItemCrystalLightBlue(int itemId) {
+		super(itemId);
+		setIconIndex(4);
+		setItemName("crystalLightBlue");
+		System.out.println(getLocalItemName(null));
+	}
+
+	
+	
+	
+		
+}
