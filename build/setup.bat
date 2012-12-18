8daea4882cb9f0c89abe3e7371f823ca9f7222c9
diesieben07 <diesieben07@gmail.com>
2012-12-18 10:53:33 +0100

    Readded build system

---
+++ build/setup.bat
@@ -0,0 +1,31 @@
+@echo off
+echo Setting up the local workspace...
+echo .
+
+echo Creating temporary forge workspace...
+rmdir /q /s temp
+md temp
+
+echo "  Unpacking forge..."
+for /r zips %%g in (*.zip) do Cscript unzip.vbs "%CD%\zips\%%~nxg" "%CD%\temp"
+
+echo "  Installing forge..."
+
+cd work\forge
+.\fml\python\python_fml install.py
+
+IF ERRORLEVEL 1 GOTO ABORT
+
+echo "  Merging forge workspace with repo..."
+
+REM Goto repo root dir
+cd ..\..\..
+
+xcopy build\work\*.* . /E /Y
+
+PAUSE
+EXIT
+:ABORT
+echo .
+echo Setup failed or aborted due to user wish.
+PAUSE
