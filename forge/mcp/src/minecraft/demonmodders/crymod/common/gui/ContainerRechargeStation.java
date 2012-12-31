cc042934f091b202ccd7a38ef20182839b52e068
diesieben07 <diesieben07@gmail.com>
2012-12-31 16:47:37 +0100

    Shift clicking for the recharge station

--- forge/mcp/src/minecraft/demonmodders/crymod/common/gui/ContainerRechargeStation.java
+++ forge/mcp/src/minecraft/demonmodders/crymod/common/gui/ContainerRechargeStation.java
@@ -1,7 +1,12 @@
 package demonmodders.crymod.common.gui;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import demonmodders.crymod.common.items.ItemCryMod;
 import demonmodders.crymod.common.tileentities.TileEntityRechargeStation;
 
 public class ContainerRechargeStation extends AbstractContainer<TileEntityRechargeStation> {
@@ -17,5 +22,52 @@ public class ContainerRechargeStation extends AbstractContainer<TileEntityRechar
         }
         
         addPlayerInventoryToContainer(playerInventory, 8, 84);
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotId) {
+		Slot slotToTransfer = getSlot(slotId);
+		if (slotToTransfer == null || !slotToTransfer.getHasStack()) {
+			return null;
+		}
+		
+		ItemStack stackToTransfer = slotToTransfer.getStack();
+		ItemStack oldStack = stackToTransfer.copy();
+		
+		if (slotId < inventory.getSizeInventory()) { // transfer from station to inventory
+			if (!mergeItemStack(stackToTransfer, inventory.getSizeInventory(), inventory.getSizeInventory() + 36, true)) {
+				return null;
+			}
+		} else { // transfer from inventory
+			if (stackToTransfer.itemID == ItemCryMod.crystal.shiftedIndex) { // merge it with one of the crystal slots
+				if (!mergeItemStack(stackToTransfer, 1, 10, false)) {
+					return null;
+				}
+			} else if (stackToTransfer.itemID == Item.ingotGold.shiftedIndex) { // merge it with the gold slot
+				if (!mergeItemStack(stackToTransfer, 0, 1, false)) {
+					return null;
+				}
+			} else if (slotId >= inventory.getSizeInventory() + 27) { // its in the hotbar
+				if (!mergeItemStack(stackToTransfer, inventory.getSizeInventory(), inventory.getSizeInventory() + 27, false)) { // merge it with the upper 3 rows
+					return null;
+				}
+			} else { // its in the upper 3 rows of the player inventory
+				if (!mergeItemStack(stackToTransfer, inventory.getSizeInventory() + 27, inventory.getSizeInventory() + 36, false)) { // merge it with the hotbar
+					return null;
+				}
+			}
+		}
+		
+		if (stackToTransfer.stackSize == 0) {
+			slotToTransfer.putStack(null); // completely transferred
+		} else {
+			slotToTransfer.onSlotChanged(); // only partially transferred
+		}
+		
+		if (stackToTransfer.stackSize == oldStack.stackSize) {
+			return null; // we cannot transfer this stack
+		}
+		
+		return oldStack;
 	}
 }
